a720c8728eda731b3651df98a6f2e675c2813509
Aleksandr Staetsky <myexploretime@gmail.com>
2019-01-04 21:34:53 +0300

    mobile version
    optimizing mobile version

---
+++ js/script.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function() {
+    $('.menu__icon').on('click', function() {
+      $(this).closest('.menu').toggleClass('menu_state_open');
+    });
+  });
+})(jQuery);
+
+
+
+
+ 
